ef1add3e2d5d80fca33255183d11e7e636bf5c1f
T0masPlata <tomrobplata@gmail.com>
2025-04-27 00:36:46 -0700

    xcode

---
+++ Vroom/Vroom/VroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  VroomApp.swift
+//  Vroom
+//
+//  Created by Tomas Plata on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct VroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
